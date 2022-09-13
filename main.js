06626c3bf504af5f8d8f0f41fd4757a746bdd146
Oskar <37629838+Badmondy@users.noreply.github.com>
2022-09-14 01:34:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+var text = "Hej jag heter Oskar Markbäck Zeilon, bor precis utanför Hässleholm med sambo och hund.";
+var text2 = "Här kommer blivande projekt dyka upp:";
+var empty  = "";
+var help = "Du kan använda dessa kommandon:<br>[project],[about],[clear],[restart],[?help],[wheater]"
+var apiKey = "6b6d7c6a50824c9193e02456220609";
+function commando() {
+   var fetched = document.getElementById("getInput").value;
+   var changed = document.getElementById("infowrite");
+   var wheatervue = document.getElementById("changeWheater");
+   var sun = document.getElementById("sunny");
+   var cold = document.getElementById("cold");
+    if(fetched == "?help"){
+       changed.innerHTML = help;
+    }else if(fetched == "about"){
+        changed.innerHTML = text; 
+        wheatervue.style.display = "none"; 
+    }
+    else if(fetched == "project"){
+        changed.innerHTML = text2;
+        wheatervue.style.display = "none";
+    }
+    else if(fetched == "clear"){
+        changed.innerHTML = empty;
+        sun.style.display = "none";
+        cold.style.display = "none";
+        wheatervue.style.display = "none";
+    }
+    else if(fetched == "restart"){
+        history.go(0);
+        
+    }else if(fetched == "wheater"){
+        window.location.href = "chatbot.html";
+        
+    }else{
+
+    }
+       
+    
+}
+
+
+function sendEmail() {
+    var link = "mailto:oskarmarkback@gmail.com";
+
+    window.location.href = link;
+}
+
+function HideData() {
+    var h = document.getElementById("proffs");
+    var p = document.getElementById("hideterminal");
+    var o = document.getElementById("hej");
+    var i = document.getElementById("iframe");
+    h.style.display = "none";
+    p.style.display = "none";
+    o.innerHTML = "BankApp Made in C#";
+    i.style.display = "flex";
+    
+}
+
+function gobackBtn(){
+    window.location.href = "index.html";
+        
+}
---
+++ wheaterAPI.js
@@ -0,0 +1,47 @@
+var apiKey = "6b6d7c6a50824c9193e02456220609";
+
+
+let url = "http://api.weatherapi.com/v1/current.json?key=6b6d7c6a50824c9193e02456220609&q=Stockholm&aqi=no";
+
+
+
+
+function GetWheater(){
+    var city = document.getElementById("city").value;
+    var sun = document.getElementById("sunny");
+    var cold = document.getElementById("cold");
+    fetch("http://api.weatherapi.com/v1/current.json?key=6b6d7c6a50824c9193e02456220609&q="+city+"&aqi=no", {
+        method: 'get'
+    })
+    .then(res => {
+        return res.json()
+      })
+      .then((response) => {
+
+        var temp = document.getElementById("wheatertext");
+        if(response.current.temp_c == 50){
+            temp.innerHTML = ("Släng dig i sjön!" + response.current.temp_c);
+            sun.style.display = "flex";
+            cold.style.display = "none";
+        }
+        else if(response.current.temp_c < 8){
+            temp.innerHTML = ("Det är kallt i "+city+ " temp: " + response.current.temp_c + " celius.");
+            sun.style.display = "none";
+            cold.style.display = "flex";
+           
+        }else if(response.current.temp_c > 8) {
+            temp.innerHTML = ("Det är varmt i "+city+ " temp: " + response.current.temp_c + " celius.");
+            sun.style.display = "flex";
+            cold.style.display = "none";
+        }else{
+            var notfound = "Den staden gick inte att hitta";
+            temp.innerHTML = notfound;
+            sun.style.display = "none";
+            cold.style.display = "none";
+        }
+        
+
+       
+        
+    })
+}

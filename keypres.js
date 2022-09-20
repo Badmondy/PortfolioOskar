0e3fc84e04e96e0a4bfbd206f7f5997603da9c32
Oskar Markbäck <oskarmarkback@gmail.com>
2022-09-20 17:32:50 +0200

    formobile

--- keypres.js
+++ keypres.js
@@ -5,7 +5,7 @@ input.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
     // Cancel the default action, if needed
-    event.preventDefault();
+
     // Trigger the button element with a click
     document.getElementById("mysecretBtn").click();
   }
--- keypres2.js
+++ keypres2.js
@@ -6,7 +6,6 @@ input2.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
     // Cancel the default action, if needed
-    event.preventDefault();
     // Trigger the button element with a click
     document.getElementById("wheaterBtn").click();
   }
